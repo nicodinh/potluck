10e28aaa255d165bc8ac07fdb333c6854e5dda9c
Andrew Joslin <andrewtjoslin@gmail.com>
2015-07-10 15:51:28 -0700

    working with resizing between two videos

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 'use strict'
+var TokboxAdapter = require('potluck-tokbox-adapter')
 
+var credentials = {
+  apiKey: '45258342',
+  sessionId: '1_MX40NTI1ODM0Mn5-MTQzNjU2NjExNDkxNX5wckUwNndrUnJ5cXdLWXlSbG44enE0dHR-fg',
+  token: 'T1==cGFydG5lcl9pZD00NTI1ODM0MiZzaWc9YzdmZGYwZGNlNzk1MzQ0MzU1MjBhZTY3MTA5M2RjMWQ3N2MzN2YwOTpzZXNzaW9uX2lkPTFfTVg0ME5USTFPRE0wTW41LU1UUXpOalUyTmpFeE5Ea3hOWDV3Y2tVd05uZHJVbko1Y1hkTFdYbFNiRzQ0ZW5FMGRIUi1mZyZjcmVhdGVfdGltZT0xNDM2NTY2MTE2Jm5vbmNlPTAuOTgzNTMxNTU2MDkyMjAyNyZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNDM2NjUyNTE2'
+}
+var adapter = new TokboxAdapter(credentials)
 
-var conference = require('./lib/conference')(document.body)
-conference.render()
+var render = require('./lib/conference')(document.body, adapter)
+render()
--- readme.md
+++ readme.md
@@ -1,45 +1,8 @@
 # potluck [![Build Status](https://travis-ci.org/eaze/potluck.svg?branch=master)](https://travis-ci.org/eaze/potluck)
 
-> Video
 
+### Development
 
-## Install
-
-```
-$ npm install --save potluck
-```
-
-
-## Usage
-
-```js
-var potluck = require('potluck');
-
-potluck('unicorns');
-//=> unicorns & rainbows
-```
-
-## API
-
-### potluck(input, [options])
-
-#### input
-
-*Required*  
-Type: `string`
-
-Lorem ipsum.
-
-#### options
-
-##### foo
-
-Type: `boolean`  
-Default: `false`
-
-Lorem ipsum.
-
-
-## License
-
-MIT © [Andrew Joslin](http://eazeup.com)
+- `npm install`
+- `npm run dev` to start watch for css/js
+- Start http server in root and open index.html
