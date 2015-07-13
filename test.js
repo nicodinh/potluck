733c7b3211ac171e4fabf5ed6bb767ffd21a26b2
Andrew Joslin <andrewtjoslin@gmail.com>
2015-07-13 00:09:29 -0700

    Remove mercury dependency

--- test.js
+++ test.js
@@ -3,8 +3,7 @@ var OpentokAdapter = require('potluck-opentok-adapter')
 var App = require('./lib')
 
 var adapter = new OpentokAdapter({
-  apiKey: process.env.OPENTOK_API_KEY,
-  // This is a test opentok sessionId and token for a test account
+  apiKey: '45258342',
   sessionId: '1_MX40NTI1ODM0Mn5-MTQzNjc0NDQ3MTU2MX55dUVNNHNmMnQ2L3NZQW9yZlRqSVdqdk5-fg',
   token: 'T1==cGFydG5lcl9pZD00NTI1ODM0MiZzaWc9Y2Y1YjI2YjJiYjQxNzE3NTFkYzBmOTNmNGQyYTMyZDQwMzk2NWI4NTpzZXNzaW9uX2lkPTFfTVg0ME5USTFPRE0wTW41LU1UUXpOamMwTkRRM01UVTJNWDU1ZFVWTk5ITm1NblEyTDNOWlFXOXlabFJxU1ZkcWRrNS1mZyZjcmVhdGVfdGltZT0xNDM2NzQ0NDcyJm5vbmNlPTAuMTE2NTYwOTM1NTA4NDU5OCZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNDM2ODMwODcy'
 })
